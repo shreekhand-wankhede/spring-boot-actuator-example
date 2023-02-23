18f27fee8cc1cda53c254ec93364cc0899ef3b5f
shreekhand.wankhede <shreekhand.wankhede@gmail.com>
2023-02-23 23:11:12 +0530

    SpringBoot actuator dependency example

---
+++ src/main/java/com/example/springbootactuatorservice/SpringBootActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootactuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootactuatorservice/SpringBootActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootactuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
